55c9b63555269acf97afc0930f802e5271a64e9d
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-04-04 12:48:53 +0200

    added filtering the items

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // functions
-function addTodo(event) {
-  event.preventDefault();
+function addTodo(e) {
+  e.preventDefault();
 
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
@@ -18,6 +20,7 @@ function addTodo(event) {
   newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
+  todoInput.value = '';
 
   const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -49,4 +52,29 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle('completed');
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
 }
